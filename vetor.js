8b8ff13e15a0d0e0a2b99a724ae7f249551e94be
LUCAS FEITOSA DE SALVO CASTRO <lucas_f_castro@estudante.sesisenai.org.br>
2023-03-15 09:10:47 -0300

    ArrayV4

--- vetor.js
+++ vetor.js
@@ -1,18 +1,17 @@
 let nome_cidade = []
-let res = document.getElementById('res').innerHTML
 
 function inserir(){
-    let cidade = document.getElementById('cidade').innerHTML
+    let cidade = document.getElementById('cidade').value
     nome_cidade.push(cidade)
     console.log(nome_cidade)
+    let res = document.getElementById('res').innerHTML
 
 }
 
 function excluir(){
-    let cidade = document.getElementById('cidade').innerHTML
+    let cidade = document.getElementById('cidade').value
     nome_cidade.pop(cidade)
     console.log(nome_cidade)
+    let res = document.getElementById('res').innerHTML
 
 }
-
-// Aaaaaaaaaaaaaaaaaa
